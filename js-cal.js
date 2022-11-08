b3c1ea7f12b247504d71f6e40352e95cfbee80c1
Huw Williams <huwmorgan91@gmail.com>
2022-11-08 14:40:25 +0000

    When a number is pressed, the number goes to sceen

--- js-cal.js
+++ js-cal.js
@@ -1,3 +1,18 @@
-function displayNumber() {
-    document.getElementById("screen").innerHTML = 1;
-}
+const screen = document.querySelector(".screen");
+const numberButtons = document.querySelectorAll(".number");
+
+    // When I press ANY number, that number should show on the screen
+    numberButtons.forEach((button) => {
+        button.addEventListener("click", (event) => {
+            screen.innerHTML = event.target.value;
+        });
+    });
+    
+
+    // Challenge: put LEFT multiple numbers on the screen
+    
+    // Challenge: put the operator as well as multiple numbers
+
+    // Challenge: put the RIGHT numbers on the screen
+
+    // CALCULATE!!!!
