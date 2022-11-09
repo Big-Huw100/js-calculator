e3af590cc85539bdcdd46613c4bc72a27fa5b415
Huw Williams <huwmorgan91@gmail.com>
2022-11-09 13:15:50 +0000

    operator buttons can now display

--- js-cal.js
+++ js-cal.js
@@ -1,7 +1,9 @@
 const screen = document.querySelector(".screen");
-const numberButtons = document.querySelectorAll(".number");
+const numberButtons = document.querySelectorAll("button");
 
-function displayNumber() {
+
+// This is how I get numbers and operators to display in the screen (but I hate it)
+function displayButton() {
     const num = document.createElement("p");
 
     const node = document.createTextNode(event.target.value);
@@ -11,8 +13,12 @@ function displayNumber() {
     document.getElementById("screen").appendChild(num);
 };
     
-    // Challenge: put the operator as well as multiple numbers
 
-    // Challenge: put the RIGHT numbers on the screen
 
-    // CALCULATE!!!!
+// Challenge: put the operator as well as multiple numbers
+
+
+// Challenge: put the RIGHT numbers on the screen
+
+
+// CALCULATE!!!!
