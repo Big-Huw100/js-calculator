936cb5207aa9057811a80593fc529176eab81a45
Huw Williams <huwmorgan91@gmail.com>
2022-11-11 12:22:16 +0000

    created all clear function.

--- js-cal.js
+++ js-cal.js
@@ -1,5 +1,6 @@
 const numberButtons = document.querySelectorAll(".number");
 const operatorButtons = document.querySelectorAll(".operator");
+const allClearButton  = document.querySelector(".allClear");
 
 let leftNumber = "";
 let operator = "";
@@ -25,15 +26,11 @@ operatorButtons.forEach(operatorButton => {
     });
 });
 
-// CHALLENGE: Populate all three of those variables so that they have values typed in
-//            by the user. When you can console.log() all three values.. We'll move on :) 
+// All clear button - return the variables to blank strings
 
-// left number - When a number is pressed, populate the "leftNumber" variable with the value. 
-//               But! ONLY if the "operator" has no value.
-
-// Operator - When an operator button is pressed, populate the "operator" variable with the value.
-
-// Right number - Only populate the right number if the leftNUmber and operator have a value 
-
-
-// RULE: Don't alter any .innerHTML for this first step. We'll do it later :) 
+allClearButton.addEventListener("click", (event) => {
+    leftNumber = "";
+    operator = "";
+    rightNumber = "";
+    console.log(leftNumber, operator, rightNumber);
+});
